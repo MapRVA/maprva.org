0e26a174fedcccb6690b263711bb2c259b2fe1b3
Daniel Schep <dschep@gmail.com>
2025-01-09 14:30:07 -0500

    hacky solution. Thanks for the help @quincylvania

--- assets/js/water-crisis-meta.js
+++
@@ -1,5 +0,0 @@
-const resp = await fetch("https://maprva.github.io/2025-water-crisis/meta.json");
-const { count, latest } = await resp.json();
-const latestDate = new Date(latest);
-document.getElementById("meta").textContent = `${count} responses as of ${latestDate.toLocaleString()}`;
-
